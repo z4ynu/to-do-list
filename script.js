edc0019075b2a3560fef424d84b20f29125084c5
Zy <zennithnunez@gmail.com>
2025-07-25 21:50:18 +0800

    fixed issue where user cant enter item and message box wont respond

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let input = document.querySelector("input");
 let output = document.querySelector(".output");
 let message = document.querySelector(".message-container");
 
-function getTodo() {
+function getTodo(value) {
   let todo = document.createElement("div");
   let textEl = document.createElement("span");
   textEl.innerHTML = value;
@@ -15,21 +15,29 @@ function getTodo() {
     message.classList.toggle("success");
   }, 2000);
 
-  let closeEl = document.createElement('span');
+  let closeEl = document.createElement("span");
   closeEl.innerHTML = "&times";
-  closeEl.classList.add('delete');
+  closeEl.classList.add("delete");
 
-  closeEl.addEventListener('click', () => {
+  closeEl.addEventListener("click", () => {
     output.removeChild(todo);
-    message.classList.toggle('error');
+    message.classList.toggle("error");
     message.textContent = "Item Deleted";
 
     setTimeout(() => {
-        message.classList.toggle("error");
+      message.classList.toggle("error");
     }, 2000);
   });
 
-    todo.appendChild(closeEl);
-    todo.classList.add('todo');
-    return todo;
+  todo.appendChild(closeEl);
+  todo.classList.add("todo");
+  return todo;
 }
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let value = input.value;
+    if (!value.trim()) return;
+    output.appendChild(getTodo(value));
+    input.value = "";
+})
